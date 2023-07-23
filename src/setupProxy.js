713ba51c9ebe835dc99bf17e5c03a22b7c01957f
eliska <eliska.novotna@teskalabs.com>
2023-07-23 23:13:25 +0200

    generalizing proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://10.17.165.188:8000', // Replace with your backend server URL
+      target: 'http://0.0.0.0:8000', // Replace with your backend server URL
       changeOrigin: true,
       pathRewrite: {
         '^/api': '',

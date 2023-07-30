5c93716bfea9e7c8886214ac1529b137826ce465
eliska <eliska.novotna@teskalabs.com>
2023-07-30 17:43:24 +0200

    unicorns pictures there

--- src/Unicorns.js
+++ src/Unicorns.js
@@ -1,8 +1,50 @@
+import { useState } from 'react';
+
 function UnicornsScreen() {
+  const [page, setPage] = useState(1)
   return (
-    <div>
-      <h1>unicorns to be here</h1>
+    <>
+    <div className="row py-4 justify-content-center">
+      <div className="col-12">
+        <div className="card shadow">
+
+          <div className="card-header">
+            <h1>How does it work?</h1>
+          </div>
+
+          <div className="card-body">
+
+            <div className="row justify-content-center">
+              <div className="col-12">
+                <img src={`./img/uni_${page.toString()}.png`} style={{maxWidth: "100%", maxHeight: "100%", objectFit: "contain"}}></img>
+                <button className="carousel-control-prev" type="button" onClick={() => {if (page>1) {setPage(page - 1)};}}>
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" onClick={() => {if (page<5) {setPage(page + 1)};}}>
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+
+            <div className="row justify-content-center">
+              <div className="col-12 col-lg-10">
+                <p className="card-text">There will be text about the unicrons.</p>
+              </div>
+            </div>
+
+            <div className="row justify-content-center">
+              <div className="col-12 col-lg-10">
+                <input id="page" type="range" className="form-range" value={page} onChange={(event) => {setPage(event.target.value);}} min="1" max="5" step="1"></input>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
+    </>
       )
 }
 
